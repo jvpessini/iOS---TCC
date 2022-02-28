d318fba8f750236fb66222380090795703b689c0
lffattori <luhf.fattori@icloud.com>
2022-02-28 20:40:51 -0300

    Initial Commit

---
+++ OCR_Scan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCR_Scan
+//
+//  Created by Luiza Fattori on 28/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
